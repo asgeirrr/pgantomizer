5445bf3af79e6e8361e49acd2a6e5193b8d411cf
Oskar Hollmann <oskar@hollmann.me>
2017-07-27 14:41:49 +0200

    0.0.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name="pgantomizer",
-    version='0.0.4',
+    version='0.0.6',
     description="Anonymize data in your PostgreSQL dababase with ease.",
     long_description=long_description,
     author='Oskar Hollmann',
